d7f489bcd1dd764231282931803ddbefb096f17b
Aquamixi <debastianierick@gmail.com>
2022-07-29 23:21:02 -0300

    envia pro banco receita

--- resources/views/criar_receitas.blade.php
+++ resources/views/criar_receitas.blade.php
@@ -4,96 +4,97 @@
 
 @section('conteudo')
     <main role="main">
-        <div class="container-fluid row ">
-            <div class="container col-6">
-                <div class="card bg-transparent border-0 ">
-                    <div class="card-body">
-                        <div class="text-center">
-                            <div class="fonteMaisFamosas"> 
+        <form method="POST" action="{{ route('cadastrar_receita') }}">
+            @csrf
+            <div class="container-fluid row mx-0">
+                <div class="container col-6">
+                    <div class="card bg-transparent border-0 ">
+                        <div class="card-body ">
+                            <div class="fonteMaisFamosas text-center"> 
                                 <h2>Titulo Da Receita</h2>                       
                             </div>
-                            <div class="">
-                                <input type="text" class="form-control fonteCriarReceitas" id="exampleFormControlInput1" placeholder="coloque aqui seu titulo">
+                            <input type="text" class="form-control fonteCriarReceitas" name="titulo" id="exampleFormControlInput1" placeholder="coloque aqui seu titulo">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid row mx-0">
+                <div class="container col-6">
+                    <div class="card bg-transparent  mb-5 border-0">
+                        <div class="card-body">
+                            <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">Adicione a foto da sua receita</h3></label>
+                            <div class="card">
+                                <div class="card-body " style="height:114px;" >
+                                    <div class="mb-3">
+                                        <label for="formFileLg" class="form-label">Large file input example</label>
+                                        <input class=" form-control position-absolute bottom-0 start-0 form-control form-control-lg" id="formFileLg" name="imagem" style="height: 114px" type="file">
+                                    </div>
+                                </div> 
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="card bg-transparent  mb-5 border-0">
-                    <div class="card-body">
-                        <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">Adicione a foto da sua receita</h3></label>
-                        <div class="card">
-                            <div class="card-body " style="height:114px;" >
-                                <div class="mb-3">
-                                    <label for="formFileLg" class="form-label">Large file input example</label>
-                                    <input class=" form-control position-absolute bottom-0 start-0 form-control form-control-lg" id="formFileLg" style="height: 114px" type="file">
-                                </div>
-                            </div> 
+                    <div class="container  d-flex justify-content-center">               
+                        <div class="mx-auto col-6 me-1">
+                            <select class="form-select" aria-label="Default select example" style="height: 50px; ">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="mx-auto col-6 ms-1">
+                            <select class="form-select" aria-label="Default select example" style="height: 50px">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="card bg-transparent mt-3 mb-5 border-0">                   
+                        <div class="card-body height:114px ">
+                            <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">descrição</h3></label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="descrva brevemente a sua receita" name="descricao" style="resize: none; height:115.7px;"></textarea>
                         </div>
                     </div>
                 </div>
-                <div class="container  d-flex justify-content-center">               
-                    <div class="mx-auto col-6 me-1">
-                        <select class="form-select" aria-label="Default select example" style="height: 50px; ">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+                <div class="container col-6">
+                    <div class="card bg-transparent mb-5 border-0">                   
+                        <div class="card-body height:114px ">
+                            <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">descrição</h3></label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="descrva brevemente a sua receita"  style="resize: none; height:115.7px;"></textarea>
+                        </div>
                     </div>
-                    <div class="mx-auto col-6 ms-1">
-                        <select class="form-select" aria-label="Default select example" style="height: 50px">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
+                    <div class="container  d-flex justify-content-center">    
+                        <div class="mx-auto col-6 me-1">
+                            <select class="form-select" aria-label="Default select example" style="height: 50px; ">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                        <div class="mx-auto col-6 ms-1">
+                            <select class="form-select" aria-label="Default select example" style="height: 50px; ">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
                     </div>
-                </div>
-                <div class="card bg-transparent mt-3 mb-5 border-0">                   
-                    <div class="card-body height:114px ">
-                        <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">descrição</h3></label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="descrva brevemente a sua "  style="resize: none; height:115.7px;"></textarea>
-                    </div>
-                </div>
-            </div>
-            <div class="container col-6">
-                <div class="d-flex  align-items-start flex-column" style="height: 116.39px;">
-                </div>
-                <div class="card bg-transparent mb-5 border-0">                   
-                    <div class="card-body height:114px ">
-                        <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">descrição</h3></label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="descrva brevemente a sua receita"  style="resize: none; height:115.7px;"></textarea>
+                    <div class="card bg-transparent mt-3 mb-5 border-0">                   
+                        <div class="card-body height:114px ">
+                            <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">descrição</h3></label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Minha receita é um bolo de cocaina"  style="resize: none; height:115.7px;"></textarea>
+                        </div>
                     </div>
                 </div>
-                <div class="container  d-flex justify-content-center">    
-                    <div class="mx-auto col-6 me-1">
-                        <select class="form-select" aria-label="Default select example" style="height: 50px; ">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div>
-                    <div class="mx-auto col-6 ms-1">
-                        <select class="form-select" aria-label="Default select example" style="height: 50px; ">
-                            <option selected>Open this select menu</option>
-                            <option value="1">One</option>
-                            <option value="2">Two</option>
-                            <option value="3">Three</option>
-                        </select>
-                    </div>
-                </div>
-                <div class="card bg-transparent mt-3 mb-5 border-0">                   
-                    <div class="card-body height:114px ">
-                        <label for="exampleFormControlTextarea1" class="form-label"><h3 class="fonteMaisFamosas">descrição</h3></label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" placeholder="Minha receita é um bolo de cocaina"  style="resize: none; height:115.7px;"></textarea>
-                    </div>
+                <div class="container text-center mb-5">
+                    <input class="btn btn-primary col-2 border-0" type="submit" value="Enviar" style="height:50px; background-color: #ff8c00; color:white">
                 </div>
             </div>
-            <div class="container text-center">
-                <input class="btn btn-primary col-2 border-0" type="submit" value="Enviar" style="height:50px; background-color: #ff8c00; color:white">
-            </div>
-        </div>
+        </form>
     </main>
     @endsection
 @section('pos-script')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/873886f170.js" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="{{asset('css/style.css?ver=3')}}">
+        <link rel="stylesheet" href="{{asset('css/style.css?ver=3.1')}}">
         @yield('extra_css')
     </head>
 
@@ -14,7 +14,7 @@
         <header class="p-1">
             <nav class="navbar navbar-expand navbar-light bg-white">
                 <div class="container-fluid">
-                    <div class="ms-0 col-md-2">
+                    <div class="col-md-2">
                         <a class="fonteMyrecipes" href="{{url('home')}}">
                             <h2>My Recipes</h2>
                         </a>
