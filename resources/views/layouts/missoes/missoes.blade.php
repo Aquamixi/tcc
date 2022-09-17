3f4d537f1683d6176a25624f4cd2ee9b2fbf4d0b
rodrigo <rodrigocaiokell@gmail.com>
2022-09-17 00:48:10 -0300

    missoes

--- resources/views/layouts/missoes/missoes.blade.php
+++ resources/views/layouts/missoes/missoes.blade.php
@@ -1,31 +1,32 @@
+<link rel="stylesheet" href="{{asset('css/style.css?ver=4.2')}}">
 <div class="text-center">
     @if (isset($um->missao))
-        <p><s>1ª Visualizar sua primeira receita.</s></p>
+        <p class="fonteMissoes"><s>1ª Visualizar sua primeira receita.</s></p>
     @else
-        <p>1ª Visualizar sua primeira receita.</p>
+        <p class="fonteMissoes">1ª Visualizar sua primeira receita.</p>
     @endif
 
     @if (isset($dois->missao))
-        <p><s>2ª Seguir um usuário.</s></p>
+        <p class="fonteMissoes"><s>2ª Seguir um usuário.</s></p>
     @else
-        <p>2ª Seguir um usuário.</p>
+        <p class="fonteMissoes">2ª Seguir um usuário.</p>
     @endif
 
     @if (isset($tres->missao))
-        <p><s>3ª Criar sua primeira receita.</s></p>
+        <p class="fonteMissoes"><s>3ª Criar sua primeira receita.</s></p>
     @else
-        <p>3ª Criar sua primeira receita.</p>
+        <p class="fonteMissoes">3ª Criar sua primeira receita.</p>
     @endif
 
     @if (isset($quatro->missao))
-        <p><s>4ª Compartilhar sua primeira receita.</s></p>
+        <p class="fonteMissoes"><s>4ª Compartilhar sua primeira receita.</s></p>
     @else
-        <p>4ª Compartilhar sua primeira receita.</p>
+        <p class="fonteMissoes">4ª Compartilhar sua primeira receita.</p>
     @endif
     
     @if (isset($cinco->missao))
-        <p><s>5ª Finalizar cadastro do seu perfil.</s></p>
+        <p class="fonteMissoes"><s>5ª Finalizar cadastro do seu perfil.</s></p>
     @else
-        <p>5ª Finalizar cadastro do seu perfil.</p>
+        <p class="fonteMissoes">5ª Finalizar cadastro do seu perfil.</p>
     @endif
 </div>
